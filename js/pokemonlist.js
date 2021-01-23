1f2154db01a5c59f8671bcc76c54f461f1e3fd15
Tim Boudreaux <tmboudreaux00@gmail.com>
2021-01-23 15:30:31 -0600

    housekeeping

--- js/pokemonlist.js
+++ js/pokemonlist.js
@@ -6,34 +6,27 @@
 // let create_el = html_tags.create_el;
 // let get_el = html_tags.get_el;
 
-//filter results
-//search bar
-
-//onSelection => y = x
-//onClick => y += y
-
-//VARIABLE INIT
-let class_pokemonCard, class_pokemonIdDiv, class_pokemonName, class_pokemonTypes, current_page, default_limit, default_offset, default_sort, first_button, get_10, get_20, get_50, get_100, get_all, get_limit, get_reverse, i, j, last_button, limit, next_button, number_of_pages, number_per_page, 
+//VAR INIT
+let arr1, arr2, check_load, check_search, class_pokemonCard, class_pokemonIdDiv, class_pokemonName, class_pokemonTypes, current_page, default_sort, filter, first_button, i, id, id_array, 
+    id_filter, j, last_button, limit, limit_record, name_array, name_filter, new_limit, new_offset, new_type_array, next_button, num_rows, number_of_pages, 
     offset, page, previous_button, pokedex, pokemon_array, pokemon_card_data, pokemon_card_photo, pokemon_card_photo_url, pokemon_id, pokemon_info,
-    pokemon_name, pokemon_types, reverse, reverse_record, sort_by_id, sort_method, sort_option, type;
-    let id_array;
-    let name_array;
-    let type_array;
-    let search_bar;
-    let new_type_array;
-    let check_search;
-    search_bar = document.getElementById('searchBar');
-    //FUNCTION INIT
-let append_pokemon, check, div, first_page, get_form, get_number_of_pages, get_pokemon, get_pokemon_type, get_user_selection, last_page, limiter, load_button, next_page, open_pokeball, page_view, 
-pokemon_id_num, pokemon_list, previous_page, set_limit, sort_pokemon, sorter, throw_pokeball;
-let pokemon_add_class, pokemon_remove_class;    
+    pokemon_name, pokemon_types, pokemon_url, reverse, reverse_record, row, search_array, search_bar, sort_by_id, sort_id, sort_method, sort_option, type, type_array, type_filter;
+    
+//FUNC INIT
+let append_pokemon, ascend_list, check, descend_list, div, first_page, get_form, get_number_of_pages, get_pokemon, get_pokemon_type, init_search, last_page, load_button, load_pokedex,
+    next_page, open_pokeball, page_view, 
+    pokemon_add_class, pokemon_remove_class, pokemon_id_num, pokemon_list, previous_page, search_pokemon, set_limit, sorter, throw_pokeball;   
 
+const get_10 = 10;
+const get_20 = 20;
+const get_50 = 50;
+const get_100 = 100;
+const get_all = 151;
 
 div = () => { return document.createElement('div'); }
 
 pokedex = document.querySelector('#pokedex');
 
-limiter = document.forms['getPokemonForm']['pokemonPerPage'];
 first_button = document.getElementById("firstPage");
 last_button = document.getElementById("lastPage");
 load_button = document.getElementById('loadButton');
@@ -44,22 +37,15 @@ class_pokemonCard = document.getElementsByClassName('pokemonCard');
 class_pokemonIdDiv = document.getElementsByClassName('pokemonIdDiv');
 class_pokemonName = document.getElementsByClassName('pokemonName');
 class_pokemonTypes = document.getElementsByClassName('pokemonTypesDiv');
+search_bar = document.getElementById('searchBar');
 
 sort_by_id = 'id';
 
-get_10 = 10;
-get_20 = 20;
-get_50 = 50;
-get_100 = 100;
-get_all = 151;
-
 current_page = 0;
 number_of_pages = 0;
-let search_array;
-let check_load
 check_load = false;
 check_search = false;
-let limit_record;
+
 id_array = new Array();
 name_array = new Array();
 pokemon_array = new Array();
@@ -76,100 +62,48 @@ let test_func = (limit, offset, reverse) => {
     console.log(`${limit} ${offset} ${reverse}`);
 }
 
-let init_search = () => {
+ascend_list = (e) => {
 
+}
+
+descend_list = (e) => {
+    
+}
+
+/** END CONSTRUCTION ZONE */
+
+init_search = () => {
     if (!check_search) {
         current_page = 1;
         check_search = true;
         check_load = false;
         load_pokedex(current_page, check_load, check_search);
     }
-    //test_func(limit, offset, reverse); //
 }
-search_bar.addEventListener('click', init_search); //if search.val != null/undefined, ignore
-
-// let id_pokemon, id_pokemonName;
-//id_pokemon = document.querySelector(`.pokemon${i}`);
-//id_pokemonName = document.querySelector(`.${p.name}`);
  
-let search_pokemon = () => { //KEYUP EVENTS
+search_pokemon = () => { //KEYUP EVENTS
     
-    //** */
-    let filter;
-    let id_filter;
-    let name_filter;
-    let type_filter;
-
-    filter = search_bar.value.toLowerCase();
-    class_pokemonName = document.getElementsByClassName('pokemonName');
     class_pokemonCard = pokedex.getElementsByClassName('pokemonCard');
-    class_pokemonTypes = document.getElementsByClassName('pokemonTypesDiv');
     class_pokemonIdDiv = document.getElementsByClassName('pokemonIdDiv');
-
-        for (i = 0; i < class_pokemonCard.length; i++) {
-            id_filter = class_pokemonIdDiv[i].textContent || class_pokemonIdDiv[i].innerHTML;
-            name_filter = class_pokemonName[i].textContent || class_pokemonName[i].innerHTML;
-            type_filter = class_pokemonTypes[i].textContent || class_pokemonTypes[i].innerHTML;
-            if (id_filter.toLowerCase().indexOf(filter) > - 1 || name_filter.toLowerCase().indexOf(filter) > - 1 || type_filter.toLowerCase().indexOf(filter) > - 1) {
-                class_pokemonCard[i].style.display = '';
-            } else {
-                class_pokemonCard[i].style.display = 'none'
-            }
+    class_pokemonName = document.getElementsByClassName('pokemonName');
+    class_pokemonTypes = document.getElementsByClassName('pokemonTypesDiv');
+    
+    filter = search_bar.value.toLowerCase();
+    
+    for (i = 0; i < class_pokemonCard.length; i++) {
+        id_filter = class_pokemonIdDiv[i].textContent || class_pokemonIdDiv[i].innerHTML;
+        name_filter = class_pokemonName[i].textContent || class_pokemonName[i].innerHTML;
+        type_filter = class_pokemonTypes[i].textContent || class_pokemonTypes[i].innerHTML;
+        if (id_filter.toLowerCase().indexOf(filter) > - 1 || name_filter.toLowerCase().indexOf(filter) > - 1 || type_filter.toLowerCase().indexOf(filter) > - 1) {
+            class_pokemonCard[i].style.display = '';
+        } else {
+            class_pokemonCard[i].style.display = 'none'
         }
-    
-    // } else if (p === 'type') {
-    //     for (i = 0; i < class_pokemonCard.length; i++) {
-
-    //         pFilter = class_pokemonTypes[i].textContent || class_pokemonName[i].textContent;
-    //         if (pFilter.toLowerCase().indexOf(filter) > - 1) {
-    //             class_pokemonCard[i].style.display = '';
-    //         } else {
-    //             class_pokemonCard[i].style.display = 'none'
-    //         }
-    // }
-    
-    // for (i = 0; i < class_filterTag.length; i++) {
-    //     if (class_pokemonCard[i] != undefined) {
-    //         name_tag = class_pokemonCard[i].getElementsByClassName('filterTag')[0];
-    //     }
-    //     //console.log(name_tag);
-    //     for (j = 1; j < 3; j++) {
-    //         if (class_pokemonCard[i] != undefined) {
-    //             new_tag = class_pokemonCard[i].getElementsByClassName('filterTag')[j];
-    //             if (new_tag != undefined) {
-    //                 type_tag = new_tag;
-    //             }    
-    //         }
-    //     }
-    //     console.log(name_tag);
-    //     console.log(type_tag);
-    // }
-    //   pokemon_remove_class(class_filterTag[i], "show");
-    //   if (class_filterTag[i].className.indexOf(p) > -1) pokemon_add_class(class_FilterTag[i], "show");
-    // }
-    
-    // for (i = 0; i < class_filterTag.length; i++) {
-    //     tag = class_filterTag[i];
-
-    //     let pid;
-    //     pid = class_pokemonCard[i].id;
-    //     if (tag != undefined && pid != undefined) {
-    //         pFilter = tag.innerText;
-    //         console.log(pid);
-            
-    //         if (pFilter.toLowerCase().indexOf(filter) > -1) {
-    //             //pid.style.display = "";
-    //             console.log(pid);
-    //         } else {
-    //         //pid.style.display = "none";
-            
-    //     } }
-    //    // console.log(tag);
-    // }  
+    }
 }
 
 pokemon_add_class = (element, name) => {
-    let arr1, arr2;
+    arr1, arr2;
     arr1 = element.className.split(' ');
     arr2 = name.split(' ');
     
@@ -180,8 +114,9 @@ pokemon_add_class = (element, name) => {
         }
     }
 }
+
 pokemon_remove_class = (element, name) => {
-    let arr1, arr2;
+    arr1, arr2;
     arr1 = element.className.split(' ');
     arr2 = name.split(' ');
     for (i = 0; i < arr2.length; i++) {
@@ -192,10 +127,11 @@ pokemon_remove_class = (element, name) => {
     element.className = arr1.join(' ');
 }
 
-search_bar.addEventListener('keyup', search_pokemon)
-
-
-/** END CONSTRUCTION ZONE */
+/**
+ * ASCEND LIST
+ * 
+ * DESCEND LIST
+ */
 
 first_page = (e) => {
     e.preventDefault();
@@ -215,7 +151,6 @@ next_page = (e) => {
     load_pokedex(current_page);
 }
 
-
 previous_page = (e) => {
     e.preventDefault();
     current_page -= 1;
@@ -230,7 +165,7 @@ get_form = (e) => {
     load_pokedex(current_page, check_load, check_search);
 }
 
-let load_pokedex = (current_page, check_load, check_search) => {
+load_pokedex = (current_page, check_load, check_search) => {
 
     limit = document.forms['getPokemonForm']['pokemonPerPage'].value;
     reverse = document.forms['getPokemonForm']['checkbox'].checked;
@@ -260,8 +195,8 @@ get_pokemon = (limit, reverse, current_page) => { //possibly remove function and
 
 set_limit = (limit, reverse, current_page) => {
     offset = 0;
-    let new_limit = limit;
-    let new_offset = offset;
+    new_limit = limit;
+    new_offset = offset;
 
     if (reverse === false) {
         switch(true) {
@@ -310,11 +245,9 @@ set_limit = (limit, reverse, current_page) => {
     page_view(new_limit, new_offset, reverse)
 }
 
-//CLEAR #POKEDEX INNER HTML AND RENDER VIEW
 page_view = (limit, offset, reverse) => {
     pokedex.innerHTML = '';
-    let row;
-    let num_rows = (Math.ceil(limit / 5));
+    num_rows = (Math.ceil(limit / 5));
 
     for (i = 0; i < num_rows; i++) {
         row = div();
@@ -337,7 +270,7 @@ pokemon_list = (limit, offset, reverse) => {
    
 throw_pokeball = (pokemon, reverse, limit) => {
 
-    let pokemon_url = pokemon.url;
+    pokemon_url = pokemon.url;
     fetch(pokemon_url)
     .then(response => response.json())
     .then(pokemon_card_data => {
@@ -358,7 +291,7 @@ open_pokeball = (pokemon_data, reverse, limit) => {
 
     pokemon_id = div();
     pokemon_id_num = () => {
-        let id = pokemon_data.id;
+        id = pokemon_data.id;
         if (id < 10)
             id = `00${id}`;
         else if (id > 9 && id < 100)
@@ -405,7 +338,7 @@ get_pokemon_type = (types, pokemon_types) => {
 };
 
 sorter = (pokemon_array, reverse, limit) => {
-    let sort_id;
+    sort_id;
     switch (true) {
         case (sort_option === sort_by_id):
             sort_id = pokemon_array.sort((a, b) => {
@@ -428,11 +361,8 @@ sorter = (pokemon_array, reverse, limit) => {
 }
 
 append_pokemon = (pokemon_array, limit) => {
-    let num_rows = (Math.ceil(limit / 5));
-    let row;
-
+    num_rows = (Math.ceil(limit / 5));
     search_array = new Array();
-
     pokemon_array.forEach((p, i) => {
         search_array.push(p.childNodes);
         for (j = 1; j < (num_rows + 1); j++){
@@ -444,7 +374,6 @@ append_pokemon = (pokemon_array, limit) => {
         }
     })  
 }
-
     get_number_of_pages = (limit) => {
     number_of_pages = Math.ceil(get_all / limit);
 }
@@ -455,8 +384,6 @@ check = () => {
     first_button.disabled = current_page == 1 ? true : false;
     last_button.disabled = current_page == number_of_pages ? true : false;
 }
-
-
 
 window.onload = () => {
     last_button.disabled = true; 
@@ -469,6 +396,9 @@ next_button.addEventListener('click', next_page);
 previous_button.addEventListener('click', previous_page);
 first_button.addEventListener('click', first_page);
 last_button.addEventListener('click', last_page);
+search_bar.addEventListener('click', init_search);
+search_bar.addEventListener('keyup', search_pokemon)
+
     /** 
  * Get the list of Pokemon from the pokemon api ​https://pokeapi.co/
  * Should have a screen that lists pokemon in a Grid Style
